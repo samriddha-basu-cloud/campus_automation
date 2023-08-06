de16616ed77f001e666aebfe9c182cd453ada2b1
Supragya Anand <74552397+Supragya2912@users.noreply.github.com>
2023-08-06 21:05:13 +0530

    Add files via upload

---
+++ CampusAutomation/src/components/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Typography } from '@mui/material'
+
+const HomePage = () => {
+    return (
+        <div>
+            <Typography>Campus Automation</Typography>
+        </div>
+    )
+}
+
+export default HomePage
